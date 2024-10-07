47918de1c267d685f5bc8ec155690362cd70f989
Manash athparia <athmanash@gmail.com>
2024-10-07 14:32:12 +0530

    Add .gitignore and package.json with express dependency and index.js with basic server setup

---
+++ index.js
@@ -0,0 +1,26 @@
+import express, {
+    json
+} from 'express';
+
+const app = express();
+app.use(json());
+
+let logs = [];
+
+app.post('/', (req, res) => {
+    if (req.query.reset) {
+        logs = [];
+    }
+
+    logs.push(req.body);
+    res.send('OK');
+});
+
+app.get('/', (req, res) => {
+    res.send(logs);
+});
+
+app.listen(8111, () => {
+    console.log('Server is running on port 3000');
+});
+
